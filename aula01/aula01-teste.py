a235f8ee7a1a3614d0e5882edd011beb1e058e7d
Enzo Miletta <milettaaa@gmail.com>
2024-03-06 22:48:29 -0300

    classroom test from python
    Just an archives for test classroom from pythom (Just important the archive aula01-teste.py

---
+++ aula01/aula01-teste.py
@@ -0,0 +1,166 @@
+"""n1 = 7
+n2 = 9
+n3 = 8
+
+print("Sua primeira nota é:", n1, "\nSua segunda nota é:", n2,"\nE sua terceira nota é", n3, "\nSua média é:", n1/3+n2/3+n3/3)
+
+#ou
+#print("A minha média é", (n1+n2+n3)/3)
+
+print("Hello World")
+
+print("olá, vamos programar?")
+
+print("Hello world")
+
+print("De novo \nBom dia")
+
+print("De novo "
+      "bom dia")
+
+print("minha idade é ",33)
+
+print("O meu nome é", "Enzo", "! Bom dia")
+
+
+msg = "A minha idade é "
+idade = 33
+print(msg, idade)
+
+a = 3
+b = 4
+print("a vale", a, "b vale", b)
+
+msg1 = "Enzo"
+msg2 = 98677
+msg3 = "ESS"
+print("Meu nome é", msg1, "Meu rm é", msg2, "E minha turma é", msg3)
+resultado1 = 10+20*30
+print(resultado1)
+resultado2 = (9**4 +2) *6-1
+print(resultado2)
+resultado3 = 10+20*30
+print(resultado3)
+
+raiz1 =27**(1/3)
+print(raiz1)
+
+resultado4 = 10%3*10**2+1-10*4/2
+print(resultado4)
+print(100-99/3%2)
+print(2*2+8/2**2)
+
+a = 3
+b = 5
+print(2*a+3*b)
+
+a =2
+b =3
+c =4
+d =a+b+c
+print(d)
+
+a = 3000
+b = 15/100
+c = a*b
+print("Seu aumento salarial é:", c)
+print("Seu salário final será de:", a+c)"""
+
+a = 1
+b = -7
+c = 10
+delta = b**2-4*a*c
+x1 = (-b+delta**(1/2))/(2)*a
+x2 = (-b-delta**(1/2))/(2)*a
+
+print(x1, x2)
+
+"""a = 2
+b = 3
+print(a+b)"""
+
+"""a, b = 2, 3
+print("a vale", a)
+print("b vale", b)"""
+
+#para fazer um comentário, utilize # (Hashtag ou sustenido)
+
+"""#Todos os comandos devem ser feitos em linhas separadas OU utilizando ; entre as linhas, 
+mas prefira sempre utilizar linhas separadas.
+"""
+#print é a primeira função vista por nós no curso, serve para exibir (imprimir) uma mensagem na tela/terminal
+
+"""
+ A função print pode ser utilizada para informar o usuário sobre:
+
+A resposta de um processamento
+o andamento da execução de um programa
+comportamentos inesperados do programa
+
+outros motivos em que o usuário precise ser informado sobre algo."""
+"""a função print()
+
+Para imprimir um texto na tela utilizamos o comando print()
+print ("De novo?")
+
+para quebrar a linha utilize \n exemplo: print("Exemplo1 \nexemplo2")
+caso fique como abaixo, será exibido na mesma linha
+print("Exemplo1"
+"exemplo2")
+
+para printar algo na tela, é possivel utilizar as aspas simples, além das aspas duplas (" ') 
+exemplo:
+print('exemplo')
+
+caso queira utilizar aspas duplas no comando exibido na tela, escreva assim:
+print(" \"Exemplo1"\")
+
+
+No meio da string pode se incluir caracteres de formação especiais
+
+o simbolo especial é \n
+
+para printar numeros, é colocado sem aspas (" ')
+exemplo: print(33)
+
+para printar um texto, é obrigatório utilizar aspas (" ')
+
+
+Quando o parâmetro do comando print for uma variável, o conteúdo do objeto é apontado 
+
+é possivel utilizar outros argumentos dentro da mesma função print
+exemplo: print("minha idade é ",33)
+ A função print pode receber infinitas funções. 
+ Exemplo:print("O meu nome é", "Enzo", "! Bom dia")
+ Para criar uma variavel, é possivel utilizar qualquer palavra, mas lembre-se de utilizar uma facil de lembrar!
+ exemplo:
+ teste = 1 
+ print(teste)
+ será exibido o numero 1
+ 
+ operadores matematicos
+ + adição
+ - subtração
+ * Multiplicação
+ / Divisão(fracionário)
+ //Divisão(inteiro)
+ %Módulo ou resto
+ **Exponenciação
+ 
+ Em expressões mantem-se a prioridade das operções como na matemática: ** -> * / // % -> +-
+ Parenteses() podem ser usados para alterar esta ordem.
+ 
+ Exponenciação: 3 elevado a 5 = 3**5
+ radiciação: raiz cubica de 27 = 3**1/3
+ 
+ atribuição de variaveis:
+ variaveis são uma forma de se associar a um nome dadop pelo programador com um objeto
+ a = 2 (a recebe 2)
+ b = 3 (b recebe 3)
+ print(a+b) (Função para mostrar valor na tela)
+ 
+ var=valor
+ var1, var2,... varN = valor1, valor2,... valorN
+ 
+ 
+"""
