00cfa575c4998c3da2b2f06840650c0b4462df00
Enzo Miletta <milettaaa@gmail.com>
2024-03-20 22:47:28 -0300

    3º classroom
    The 3º classroom from python ( Any things funny about it)

---
+++ Aula 03.py
@@ -0,0 +1,61 @@
+"""nota1 = 5;
+nota2 = 4;
+nota3 = 5;
+resultado = nota1 + nota2 + nota3;
+
+if resultado > 7.0:
+    print("Sua nota foi maior que a média. Você tirou", resultado);
+    if resultado < 7.0:
+       print("Sua nota foi menor que a média. Você tirou", resultado);
+como professor faria:
+materia1 = 7
+materia2 = 5
+materia3 = 6
+podersupremo = 0
+
+resultado = (materia1 >= 7 and materia2 >=7 and materia3 >=7) or podersupremo == 1
+
+print(resultado)
+a = 'Hoje tem aula!'
+print()
+print(a[:])
+a = 'Enzo Miletta Herrera da Silva'
+
+print(a[::2], len(a))
+a = 'Hoje é um dia muito legal'
+print(a [1::2])
+print(a[::3])
+a = 'Enzo'
+b = 'Miletta'
+print(a[1:] + b[1:])
+f-strings:
+Não precisa identificar o tipo
+se quiser especificar o tipo utilizar:{variavel:}
+após o : a regra é igual ao format
+ex:{variável:_^10.2f}
+para utilizar a f string:
+nome1 = maria
+ano = 2000
+print(f"{nome1} e {'Joao'} nasceram em {ano}"}
+para quebrar uma linha utilize /n
+função input()
+Realiza a leitura de dados a partir do teclado
+aguarda que o usuario digite o valor, e após teclar enter, será inserido o valor na memória
+
+=====================================================//======================================================
+
+x = input('Digite seu nome:')
+print("Seu nome é:", x)
+x = input('Digite seu nome:')
+y = input('Digite sua idade:')
+print(f"Meu nome é:", x, "e minha idade é", y)
+Exemplo de calculo utilizando input (int para transformar a função input em numero inteiro)
+x = int(input("Digite um numero Inteiro:"))
+y = int(input("Digite o segundo numero inteiro:"))
+soma = (x + y)
+
+print("A Soma de", x, "+", y, "é:", soma)
+
+"""
+
+
