6a64521b3279300400fe5383d5a2919f1035ef18
Enzo Miletta <161778417+Enzo-Miletta@users.noreply.github.com>
2024-03-27 22:49:30 -0300

    4º Classroom
    My 4º classroom of python (Fudeu cp)

---
+++ Aula04.py
@@ -0,0 +1,46 @@
+"""a = int(input('Digite o valor de A: '))
+b = int(input('Digite o valor de B: '))
+if a > b:
+    print('a é maior que b')
+if a <= b:
+    print('a Não é maior que b')
+
+a = int(input('Digite a velocidade do seu carro: '))
+if a > 80:
+    print("Seu carro está muito rapido, você recebeu uma multa de 5 (CINCO) Reais por kilometro")
+    print("Você recebeu uma multa de ", (a-80)*5, "reais.")
+if a <= 80:
+    print("Você está dentro da lei. Parabéns!")
+
+a = int(input("Digite o valor de A: "))
+b = int(input("Digite o valor de B: "))
+
+if a > b:
+    print("A é maior que B")
+else:
+    print("B é maior que A")
+
+a = int(input("Digite o seu numero: "))
+if a % 2 == 0:
+    print("Seu numero ou é par")
+else:
+    print("Seu numero é impar")
+if a > 100:
+    print("Seu numero é maior ou igual que 100")
+else:
+    print("Seu numero é menor que 100")"""
+a = int(input("Digite um numero:"))
+if a < 100:
+    if a % == 0:
+        print("Seu numero é Par e maior ou igual a 100")
+else:
+    print("Seu numero é impar e maior que 100")
+    else:
+    if a > 100:
+            if a % == 0:
+                print("Seu ")
+
+
+
+
+
